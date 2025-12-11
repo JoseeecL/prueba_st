f515eed8bb14e9a8743079a0bfa03cffc9dedbf9
JoseeecL <81941286+JoseeecL@users.noreply.github.com>
2025-12-11 17:57:15 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 
 st.title("Taller ML")
 st.write("Hola")
 st.button("Botonnn")
+
 
 
